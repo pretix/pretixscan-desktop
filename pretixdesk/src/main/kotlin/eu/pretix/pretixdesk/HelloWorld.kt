03ddf4dd7a8f71b52b8f242dcdca4d4f5669f9db
Raphael Michel <mail@raphaelmichel.de>
2017-11-05 12:43:10 +0100

    Inital commit

---
+++ pretixdesk/src/main/kotlin/eu/pretix/pretixdesk/HelloWorld.kt
@@ -0,0 +1,11 @@
+package eu.pretix.pretixdesk
+import com.jfoenix.controls.JFXButton
+import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.HBox
+import tornadofx.*
+
+
+class HelloWorld : View() {
+    override val root: HBox by fxml()
+
+}
---
+++ pretixdesk/src/main/kotlin/eu/pretix/pretixdesk/PretixDeskMain.kt
@@ -0,0 +1,7 @@
+package eu.pretix.pretixdesk
+import tornadofx.*
+
+
+class PretixDeskMain : App() {
+    override val primaryView = HelloWorld::class
+}
