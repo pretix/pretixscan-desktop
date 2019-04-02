d2c43446acb0ad6254242607fce0cf88c294a195
Raphael Michel <mail@raphaelmichel.de>
2019-04-02 17:48:18 +0200

    Support SSL workaround

--- pretixscan/src/main/kotlin/eu/pretix/pretixscan/desktop/OkHttpClientFactory.kt
+++ pretixscan/src/main/kotlin/eu/pretix/pretixscan/desktop/OkHttpClientFactory.kt
@@ -1,11 +1,8 @@
 package eu.pretix.pretixscan.desktop
 
+import eu.pretix.libpretixsync.api.DefaultHttpClientFactory
 import eu.pretix.libpretixsync.api.HttpClientFactory
 import okhttp3.OkHttpClient
 
-class OkHttpClientFactory : HttpClientFactory {
-    override fun buildClient(): OkHttpClient {
-        return OkHttpClient.Builder()
-                .build()
-    }
+class OkHttpClientFactory : DefaultHttpClientFactory() {
 }
