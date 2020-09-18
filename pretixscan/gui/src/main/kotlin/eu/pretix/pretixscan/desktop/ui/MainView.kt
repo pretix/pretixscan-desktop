dbf82ca70ac5b2d37e7596f27b61f500dffcd0ac
Raphael Michel <mail@raphaelmichel.de>
2020-09-18 16:28:27 +0200

    Clarify entry/exit toggle

--- pretixscan/gui/src/main/kotlin/eu/pretix/pretixscan/desktop/ui/MainView.kt
+++ pretixscan/gui/src/main/kotlin/eu/pretix/pretixscan/desktop/ui/MainView.kt
@@ -285,6 +285,7 @@ class MainView : View() {
                     style {
                         alignment = Pos.CENTER_RIGHT
                     }
+                    label(messages["toolbar_toggle_entry"])
                     val conf = (app as PretixScanMain).configStore
                     toggleExit = jfxTogglebutton(if(conf.scanType == "exit") messages["toolbar_toggle_exit"] else messages["toolbar_toggle_entry"]) {
                         toggleColor = c(STYLE_STATE_VALID_COLOR)
@@ -297,7 +298,7 @@ class MainView : View() {
                                 conf.scanType = "exit"
                             }
                             toggleExit?.isSelected = conf.scanType == "exit"
-                            toggleExit?.text = if(conf.scanType == "exit") messages["toolbar_toggle_exit"] else messages["toolbar_toggle_entry"]
+                            toggleExit?.text = messages["toolbar_toggle_exit"]
                         }
                     }
                     this += toggleExit!!
