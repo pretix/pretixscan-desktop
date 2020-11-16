04f18182e2cd9b9ed40991ff860626c7d3073587
Raphael Michel <mail@raphaelmichel.de>
2020-11-16 14:17:53 +0100

    Bump version

--- pretixscan/gui/src/main/kotlin/eu/pretix/pretixscan/desktop/PretixScanMain.kt
+++ pretixscan/gui/src/main/kotlin/eu/pretix/pretixscan/desktop/PretixScanMain.kt
@@ -33,8 +33,8 @@ import java.util.*
 import java.util.concurrent.locks.ReentrantLock
 
 
-val VERSION = "1.5.1"
-val VERSION_CODE = 8
+val VERSION = "1.8.0"
+val VERSION_CODE = 9
 val APP_ID = "eu.pretix.pretixscan.desktop"
 
 class PretixScanMain : App(MainView::class, MainStyleSheet::class) {
